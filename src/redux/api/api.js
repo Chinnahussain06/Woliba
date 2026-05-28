51c720be6bf86630d166d7982442c2ad05a732c0
Chinna hussain vali Shaik <53077867+Chinnahussain06@users.noreply.github.com>
2026-05-28 14:19:02 +0530

    env files fixed

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "/v1"
-      : "https://dev.api.woliba.io/v1",
-
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
